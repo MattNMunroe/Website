5dcb8845b49481f0925fec3c826cdb40dd5c5263
Matt M <Afrodesiacc@gmail.com>
2024-01-09 15:22:33 -0500

    Adding existing project files to Git.

---
+++ Website/frontend/website/src/Components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h2> Welcome and Hello.</h2>
+            <break></break>
+            <h3> This website is a page dedicated to keeping aspects of my consciousness infinite and cataloging ideas as well as sharing artwork. Please feel free to browse my work, and if you feel so inclined, purchasing some using our Cart. Also, I do recommend checking out the section of this site for the Occult as its filled with vital information.</h3>
+        </div>
+    )
+}
+
+export default About
---
+++ Website/frontend/website/src/Components/Occult/occult.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Occult = () => {
+    return (
+        <div>
+            <h2>"One who knows nothing, can understand nothing." - Ansem, Seeker of Darkness</h2>
+            <break></break>
+            <h4>Concerning the Occult, most get too weirded out or terrified before actually knowing what it is they're so adversed to.</h4>
+        </div>
+    )
+}
+
+export default Occult
